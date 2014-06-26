32e1a919f7d5b6e4f697e402b2717b2e3441e939
James Hilliard <james.hilliard1@gmail.com>
2014-06-26 12:07:47 -0600

    validate presence

--- app/models/spree/bank_account.rb
+++ app/models/spree/bank_account.rb
@@ -1,9 +1,9 @@
 module Spree
   class BankAccount < ActiveRecord::Base
-    validates :bank_name
-    validates :bank_branch
-    validates :account_owner
-    validates :account_number
+    validates :bank_name, presence: true
+    validates :bank_branch, presence: true
+    validates :account_owner, presence: true
+    validates :account_number, presence: true
     scope :active, -> { where(active: true) }
   end
 end
