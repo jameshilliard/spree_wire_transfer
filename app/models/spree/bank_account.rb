23da680f07288dc9eb72e2fe758da2d28492df9b
James Hilliard <james.hilliard1@gmail.com>
2014-06-26 12:04:29 -0600

    update model

--- app/models/spree/bank_account.rb
+++ app/models/spree/bank_account.rb
@@ -1,16 +1,9 @@
 module Spree
   class BankAccount < ActiveRecord::Base
-    attr_accessor :bank_name
-    attr_accessor :bank_branch
-    attr_accessor :account_owner
-    attr_accessor :account_number
-    attr_accessor :short_code
-    attr_accessor :swift_code
-
-    validate :bank_name
-    validate :bank_branch
-    validate :account_owner
-    validate :account_number
+    validates :bank_name
+    validates :bank_branch
+    validates :account_owner
+    validates :account_number
     scope :active, -> { where(active: true) }
   end
 end
